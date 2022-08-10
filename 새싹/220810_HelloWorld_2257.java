bb9c582a6381e33be2b69ee54790ccc15ec0e650
jindol <parkmoc21@gmail.com>
2022-08-10 20:49:01 +0900

    22.08.10 algorithm solved.ac BE IN PROGRESSING

---
+++ 새싹/220810_HelloWorld_2257.java
@@ -0,0 +1,9 @@
+public class 220810_HelloWorld_2257 {
+    void String printHelloWorld() {
+        system.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
