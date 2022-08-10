a6d9cf46c75011e80d8ba2ad98f18cf75d28e03a
jinDol99 <parkmoc21@gmail.com>
2022-08-10 22:01:06 +0900

    new file: BJ220810_2557_HelloWorld.java deleted: 220810_HelloWorld_2257.java
    파일 이름 오류(숫자로 시작)로 디버그가 안되던 문제 해결
    solved.ac 새싹 2557번 Hello World 해결!

--- 새싹/220810_HelloWorld_2257.java
+++
@@ -1,9 +0,0 @@
-public class 220810_HelloWorld_2257 {
-    void String printHelloWorld() {
-        system.out.println("Hello World!");
-    }
-
-    public static void main(String[] args) {
-        printHelloWorld();
-    }
-}
---
+++ 새싹/BJ220810_2557_HelloWorld.java
@@ -0,0 +1,12 @@
+class printString {
+    void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
+
+public class BJ220810_2557_HelloWorld {
+    public static void main(String[] args) {
+        printString print = new printString();
+        print.printHelloWorld();
+    }
+}
