efe7988f303e612abfcef8b51b0885f9f5d04120
jinDol99 <parkmoc21@gmail.com>
2022-08-10 22:28:35 +0900

    new file: BJ220810_10699_todayDate.java build: BJ220810_2557_HelloWorld.java
    HelloWorld.java 파일의 주석을 추가
    solved.ac 새싹 10699번 오늘 날짜 해결!

--- 새싹/BJ220810_2557_HelloWorld.java
+++ 새싹/BJ220810_2557_HelloWorld.java
@@ -1,12 +1,12 @@
-class printString {
-    void printHelloWorld() {
-        System.out.println("Hello World!");
-    }
+class printString {                             // 새 클래스 생성: printString
+    void printHelloWorld() {                    // 리턴값이 없는(void) printHelloWorld() 메소드를 생성
+        System.out.println("Hello World!"); // 해당 메소드는 "Hello World"를 출력하는 문장을 실행하는
+    }                                           // 역할을 지님.
 }
 
 public class BJ220810_2557_HelloWorld {
-    public static void main(String[] args) {
-        printString print = new printString();
-        print.printHelloWorld();
+    public static void main(String[] args) {    // main 메소드를 생성
+        printString print = new printString();  // printString 클래스를 선언
+        print.printHelloWorld();                // printHelloWorld() 메소드를 실행
     }
 }
