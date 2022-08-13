2b11fe026e773fd1818da685f1ed7d4d76d2f4c0
jindol <parkmoc21@gmail.com>
2022-08-13 18:29:19 +0900

    main 커밋

--- 새싹/gitTest.java
+++ 새싹/gitTest.java
@@ -1,9 +1,5 @@
 public class gitTest {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("This is MAIN branch");
-=======
-        System.out.println("This is yourBranch branch");
->>>>>>> yourBranch
     }
 }
