3f14b8d77f545c5e0948627e0cc327a35928eec6
jindol <parkmoc21@gmail.com>
2022-08-14 19:45:42 +0900

    branchTest 브랜치에서  2번째 커밋(Rebase)

--- 새싹/gitTest.java
+++ 새싹/gitTest.java
@@ -1,5 +1,6 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("I'm Testing in 'branchTest' Branch");
+        System.out.println("branchTest 브랜치에서 작업 중이에옹");
     }
 }
