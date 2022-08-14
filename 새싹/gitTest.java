20a3619a6abe8ab97437107ffb4011d69cba7c9d
jindol <parkmoc21@gmail.com>
2022-08-14 19:41:13 +0900

    branchTest 브랜치 커밋(Rebase)

--- 새싹/gitTest.java
+++ 새싹/gitTest.java
@@ -1,5 +1,5 @@
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("This is main branch!");
+        System.out.println("I'm Testing in 'branchTest' Branch");
     }
 }
