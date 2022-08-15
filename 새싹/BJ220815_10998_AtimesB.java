3396f8a6cd13b02e77c1578679b5247d0bd2db80
jindol <parkmoc21@gmail.com>
2022-08-15 23:42:14 +0900

    new file: BJ220815_10998_AtimesB.java
    solved.ac 새싹 10998번 A×B 문제 해결!

---
+++ 새싹/BJ220815_10998_AtimesB.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class BJ220815_10998_AtimesB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a*b);
+        sc.close();
+    }
+}
