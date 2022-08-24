4482142a5715a21624b5468acb5327aeb71e1532
jindol <parkmoc21@gmail.com>
2022-08-24 17:26:56 +0900

    modified: BJ220824_14681_Quadrant.java
    설명 주석 추가

--- 새싹/BJ220824_14681_Quadrant.java
+++ 새싹/BJ220824_14681_Quadrant.java
@@ -15,9 +15,12 @@ public class BJ220824_14681_Quadrant {
                 System.out.println(3);
         }
     }
+    // whatQuadrantIsIt() 메소드 - x, y 두 개의 값을 가져와 음수와 양수인지 판단하여
+    //                            상황에 맞는 값을 출력함
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         whatQuadrantIsIt(sc.nextInt(), sc.nextInt());
         sc.close();
     }
+    // 메인 메소드 - 값을 입력받아 whatQuad...It() 메소드에 넘겨줌
 }
