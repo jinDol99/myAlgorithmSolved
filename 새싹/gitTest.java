55e3656a6648830a87d7bf22888529a86e30f196
jinDol99 <parkmoc21@gmail.com>
2022-11-30 00:21:37 +0900

    221130 00:21 git 폴더명 변경 테스트

--- 새싹/gitTest.java
+++ 새싹/gitTest.java
@@ -1,5 +1,6 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("I'm Testing in main Branch");
+        System.out.println("testing for change directory name!");
     }
 }
