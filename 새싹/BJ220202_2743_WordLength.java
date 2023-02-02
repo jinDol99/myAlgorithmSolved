e0e3bfdfda359650720dd2c6b484d07c39249647
jindol <parkmoc21@gmail.com>
2023-02-02 16:46:12 +0900

    new file: BJ220202_2743_WordLength.java 외 1개
    백준 문자열 2743번 단어 길이 재기 문제 완료!
    코틀린 방식으로의 문제는 미해결

---
+++ 새싹/BJ220202_2743_WordLength.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BJ220202_2743_WordLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().length());
+        sc.close();
+    }
+}
